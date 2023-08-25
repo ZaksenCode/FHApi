c326606299a0515ca64c585784961689975afeb5
Xemii16 <balamutmaks@gmail.com>
2023-08-25 13:53:02 +0300

    Added database error logging

--- src/main/java/org/zaksen/fhapi/database/DatabaseManager.java
+++ src/main/java/org/zaksen/fhapi/database/DatabaseManager.java
@@ -3,6 +3,10 @@ package org.zaksen.fhapi.database;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.TextDisplay;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.zaksen.fhapi.FHApi;
 import org.zaksen.fhapi.data.LoadedHologram;
 import org.zaksen.fhapi.text.Hologram;
 
@@ -12,52 +16,52 @@ import java.util.UUID;
 
 public class DatabaseManager {
     public static final DatabaseManager Instance = new DatabaseManager();
-
-    private final Connection connection;
-    private final Statement statement;
+    private final Logger logger;
+    private Connection connection;
+    private Statement statement;
 
     private DatabaseManager() {
+        logger = LoggerFactory.getLogger(FHApi.class);
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:holograms.sqlite");
             statement = connection.createStatement();
             statement.execute("CREATE TABLE IF NOT EXISTS ids(id INTEGER, uuid TEXT);");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            logger.error("A database connection could not be established", e);
         }
     }
 
-    public void addHologram(Hologram holo) {
+    public void addHologram(@NotNull Hologram holo) {
         try {
             statement.execute(String.format("INSERT INTO ids(id, uuid) VALUES('%s', '%s');", holo.getId(), holo.getUUID().toString()));
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            logger.error("Failed to add hologram to database", e);
         }
     }
 
-    public void removeHologram(Hologram holo) {
+    public void removeHologram(@NotNull Hologram holo) {
         try {
             statement.executeUpdate(String.format("DELETE FROM ids WHERE id = %s", holo.getId()));
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            logger.error("Failed to delete hologram to database", e);
         }
     }
 
-    public HashMap<Integer, LoadedHologram> getHolograms()
-    {
+    public HashMap<Integer, LoadedHologram> getHolograms() {
         HashMap<Integer, LoadedHologram> result = new HashMap<>();
         ResultSet messageSet;
         try {
             messageSet = statement.executeQuery("SELECT * FROM ids;");
-            while(messageSet.next()) {
+            while (messageSet.next()) {
                 int id = messageSet.getInt("id");
                 String uuid = messageSet.getString("uuid");
                 Entity loadedEntity = Bukkit.getEntity(UUID.fromString(uuid));
-                if(loadedEntity instanceof TextDisplay) {
+                if (loadedEntity instanceof TextDisplay) {
                     result.put(id, new LoadedHologram(id, (TextDisplay) loadedEntity));
                 }
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            logger.error("Failed to retrieve holograms from database", e);
         }
         return result;
     }
